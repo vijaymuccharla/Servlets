b6368c91f82f3ff279518989f1462582cf8976fc
vkmuc <vkmuc@Dell-Inspiron>
2020-04-22 01:49:28 +0530

    Send-Redirection-

---
+++ SendRedirection-sendRedMethod/src/com/vj/servlet/SearchServlet.java
@@ -0,0 +1,50 @@
+package com.vj.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/searchurl")
+public class SearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public SearchServlet() {
+    	System.out.println("SearchServlet.SearchServlet()");
+    }
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("SearchServlet.doGet()");
+		PrintWriter pw=null;
+		String query=null,engine=null;
+		//get writer from response
+		pw=resp.getWriter();
+		//set respose content type
+		resp.setContentType("text/html");
+		//read form data
+		query=req.getParameter("query");
+		engine=req.getParameter("engine");
+		
+		if(engine.equalsIgnoreCase("google")) 
+				resp.sendRedirect("https://www.google.com/search?q="+query);
+		else if(engine.equalsIgnoreCase("yahoo")) 
+			resp.sendRedirect("https://in.search.yahoo.com/search?p="+query);
+		else if(engine.equalsIgnoreCase("bing")) 
+			resp.sendRedirect("https://www.bing.com/search?q="+query);
+		
+			//close stream
+		pw.close();
+		
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("SearchServlet.doPost()");
+		doGet(request, response);
+	}
+
+}
